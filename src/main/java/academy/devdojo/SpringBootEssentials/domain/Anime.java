4b6bfa3be47d5be12af8acfa6fd0082c29d668ca
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-16 17:09:20 -0300

    Field Validation

--- src/main/java/academy/devdojo/SpringBootEssentials/domain/Anime.java
+++ src/main/java/academy/devdojo/SpringBootEssentials/domain/Anime.java
@@ -17,6 +17,7 @@ public class Anime {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     private String name;
 
 }
--- src/main/java/academy/devdojo/SpringBootEssentials/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/SpringBootEssentials/requests/AnimePostRequestBody.java
@@ -1,9 +1,13 @@
 package academy.devdojo.SpringBootEssentials.requests;
 
 import lombok.Data;
+import org.springframework.lang.Nullable;
+
+import javax.validation.constraints.NotEmpty;
 
 @Data
 public class AnimePostRequestBody {
 
+    @NotEmpty(message = "The anime name cannot be empty")
     private String name;
 }
