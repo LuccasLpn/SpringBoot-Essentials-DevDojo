aeef621bcb63959b87a1607f8f5ad2b4211828e1
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-16 16:41:05 -0300

    BadRequestException Handler

---
+++ src/main/java/academy/devdojo/SpringBootEssentials/exception/BadRequestExceptionDetails.java
@@ -0,0 +1,20 @@
+package academy.devdojo.SpringBootEssentials.exception;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class BadRequestExceptionDetails {
+
+    private String title;
+    private int status;
+    private String details;
+    private String developerMessage;
+    private LocalDateTime timestamp;
+
+
+}
