473e2be8ae081460d347ad1e0d41bf8df9222b58
Luccas Pereira <Luccas.lpn@outlook.com>
2022-01-21 20:12:49 -0300

    Grafana

--- src/main/java/academy/devdojo/SpringBootEssentials/SpringBootEssentialsApplication.java
+++ src/main/java/academy/devdojo/SpringBootEssentials/SpringBootEssentialsApplication.java
@@ -1,8 +1,12 @@
 package academy.devdojo.SpringBootEssentials;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +17,11 @@ public class SpringBootEssentialsApplication {
 
 		SpringApplication.run(SpringBootEssentialsApplication.class, args);
 
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(
+			@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
 	}
 }
